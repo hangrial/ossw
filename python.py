44dfa9fe1b4b4fd0eeb56698ee6be4dd12d383fa
hangrial <roads.fluent0z@icloud.com>
2024-09-12 17:30:50 +0900

    commit

--- python.py
+++
@@ -1,6 +0,0 @@
-{\rtf1\ansi\ansicpg949\cocoartf2818
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww35500\viewh21080\viewkind0
-}
